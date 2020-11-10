bd19045dd119ba3936d73ea41cec1fc6ea4d752b
Rizky Tri Sulistyo <rizkytrisulistyo@gmail.com>
2020-11-10 23:40:52 +0700

    add palindrom and anagram test

--- latihan/string-utils.js
+++ latihan/string-utils.js
@@ -1,10 +1,10 @@
-const isPalindrome = (string) => string == string.split('').reverse().join('');
+const isPalindrome = (string) => string == string.split("").reverse().join("");
 
 const isAnagram = (w1, w2) => {
-    const regularize = (word) = {
-        return word.toLowerCase().split('').sort().join('').trim();
-    }
-    return regularize(w1) === regularize(w2);
-}
+  const regularize = (word) => {
+    return word.toString().toLowerCase().split("").sort().join("").trim();
+  };
+  return regularize(w1) === regularize(w2);
+};
 
-module.exports = {isPalindrome, isAnagram}
+module.exports = { isPalindrome, isAnagram };
